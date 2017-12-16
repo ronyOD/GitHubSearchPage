8dcd7c7a9a54dae1640b1d1376d98d010399f1dc
ronyOD <ronyais1989@gmail.com>
2017-12-16 10:16:26 -0800

    Start Up Page

--- GitHubSearchPage/App_Start/RouteConfig.cs
+++ GitHubSearchPage/App_Start/RouteConfig.cs
@@ -12,6 +12,7 @@ namespace GitHubSearchPage
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.IgnoreRoute("");
 
             routes.MapRoute(
                 name: "Default",
